17ad27d080b58399db42e3bdc8e54721753348ea
Mateusz <matrys000@utp.edu.pl>
2025-03-03 17:54:48 +0100

    AesApi V.1.2

--- AesApi/Controllers/AesController.cs
+++ AesApi/Controllers/AesController.cs
@@ -10,6 +10,7 @@ namespace AesApi.Controllers
     public class AesController : ControllerBase
     {
         [HttpPost("encrypt")]
+        [EndpointSummary("Encrypt text with AES algorythm, using provided key.")]
         public ActionResult<string> EncryptWithAes([FromBody] EncryptionModel encryptionModel)
         {
             try
@@ -23,6 +24,7 @@ namespace AesApi.Controllers
         }
 
         [HttpPost("decrypt")]
+        [EndpointSummary("Decrypt text with AES algorythm, using provided key.")]
         public ActionResult<string> DecryptWithAes([FromBody] DecryptionModel decryptionModel)
         {
             try
@@ -36,10 +38,10 @@ namespace AesApi.Controllers
         }
 
         [HttpGet("generateKey/{keySize}")]
+        [EndpointSummary("Generates a Base64-encoded AES key of the specified size.")]
         public string GenerateBase64Key(int keySize)
         {
             return AesService.GenerateBase64Key(keySize);
         }
-    
     }
 }
